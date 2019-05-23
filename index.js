61d3047a79107fca60ec62a97d24ba0db375fc62
Sam Calamos <scalamos@nine.com.au>
2019-05-23 16:22:56 +1000

    heehee

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const login = require("facebook-chat-api");
+const fs = require("fs");
 require('dotenv').config();
 
 
-login({email: process.env.FB_USER, password: process.env.FB_PASS}, (err, api) => {
+login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
     if(err) return console.error(err);
+
+    // fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
 
     api.listen((err, message) => {
 
-        if (message.body.toLowerCase() == "lol") {
+        if (message.body.toLowerCase().split(" ").includes("lol")) {
             console.log(message);
             console.log("sending reaction...")
             api.setMessageReaction("😠", message.messageID);
         }
+
     })
 })
