f35ad45c028a4e0c6fc238f9681756f03fbf9408
Sam Calamos <scalamos@nine.com.au>
2019-05-24 13:11:06 +1000

    Making dynamic updates

--- index.js
+++ index.js
@@ -2,12 +2,50 @@ const login = require("facebook-chat-api");
 const fs = require("fs");
 require('dotenv').config();
 
+// Try log in with current app state otherwise use credentials
+try {
+    const appState = JSON.parse(fs.readFileSync('appstate.json', 'utf8'))
+} catch (e) {
+    console.error(e);
+}
 
-login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
-    if(err) return console.error(err);
+// If appState exists login with it
+if(appState) {
+    logInWithAppState(appState);
+} else {
+    logInWithCredentials();
+}
 
-    // fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
 
+function logInWithCredentials() {
+    console.log("Loggin in with credentials from env variables...");
+    login({email: process.env.FB_USER, password: process.env.FB_PASS}, (err, api) => {
+        if(err) return console.error(err);
+
+        // save app state
+        console.log("Writing app state to file appstate.json")
+        fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+
+        startListeningForMessages(api);
+    })
+}
+
+function logInWithAppState(appState) {
+    console.log("Logging in with pre-existing appstate")
+    login({appState: appState }, (err, api) => {
+        if(err) {
+            console.error(err);
+            console.log("Trying to log in with credentials...");
+            
+            logInWithCredentials()
+    
+        } else {
+            startListeningForMessages(api);
+        }    
+    })
+}
+
+function startListeningForMessages(api) {
     api.listen((err, message) => {
 
         if (message.body.toLowerCase().split(" ").includes("lol")) {
@@ -17,4 +55,4 @@ login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, ap
         }
 
     })
-})
+}
