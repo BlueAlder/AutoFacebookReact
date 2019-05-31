3e43e037d131d562bce832cd444573db0bc51c6a
Sam Calamos <sam_calamos@hotmail.com>
2019-05-31 15:51:41 +1000

    adding more whitelist

--- src/data/whitelist.js
+++ src/data/whitelist.js
@@ -1,15 +1,16 @@
 const Constants = require("./constants");
 
 
-const whitelisted_photo_senders = [Constants.senderID["erol"], Constants.senderID["grace"]];
-const whitelisted_photo_threads = [Constants.threadID["bangme"]];
+const whitelisted_photo_senders = [Constants.senderID["erol"], Constants.senderID["grace"], Constants.senderID["sam"]];
+const whitelisted_photo_threads = [Constants.threadID["bangme"], Constants.threadID["gfBang"]];
 
 const whitelisted_sentiment_senders = [Constants.senderID["erol"], Constants.senderID["grace"], Constants.senderID["sam"]];
 const whitelisted_sentiment_threads = [Constants.threadID["bangme"],
                                         Constants.threadID["ass"],
                                         Constants.threadID["gfBang"],
                                         Constants.threadID["BIT"],
-                                        Constants.threadID["kiama"]];
+                                        Constants.threadID["kiama"],
+                                        Constants.threadID["family"]];
 
 const whitelisted_angry_senders = [Constants.senderID["grace"], Constants.senderID["sam"]];
 const whitelisted_angry_threads = [];
